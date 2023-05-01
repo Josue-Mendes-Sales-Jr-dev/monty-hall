fce7675157642d7f7dec3522690735861d3b824d
Josué Mendes <109279870+Josue-Mendes-Sales-Jr-dev@users.noreply.github.com>
2023-05-01 13:40:37 -0300

    add events

--- src/components/porta/index.tsx
+++ src/components/porta/index.tsx
@@ -8,14 +8,31 @@ interface IPortaProps{
 export const Porta=(props:IPortaProps)=>{
     const porta=props.value
     const selected= props.value.selecionada? styles.selecionada : ""
-    
+   const abrir=(e:any)=>{
+    e.stopPropagation()
+    props.onChangee(porta.abrir())
+    console.log('click')
+   }
     const  alternarSelecao=(e: any)=>props.onChangee(porta.alternarSelecao())
+    
+    const renderiza=()=>{
+        return(
+            <>  
+                <div className={styles.box}>
+                <div className={styles.numero}>3</div>
+                <div className={styles.macaneta}  onClick={abrir}></div>
+                </div>
+           
+            
+            </>
+        )
+    }
+  
     return(
         <>
-        <div className={styles.box} >
-            <div className={`${styles.area} ${selected}`} onClick={alternarSelecao}>
-                <div className={styles.numero}>3</div>
-                <div className={styles.macaneta}></div>
+        <div className={styles.area} onClick={alternarSelecao} >
+           <div className={`${styles.estrutura} ${selected}`} >
+            {porta.aberta ? false : renderiza()}
             </div>
             <div className={styles.chao}></div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import {Presente} from '../components/presentes'
 import { Porta } from '../components/porta'
@@ -6,12 +7,12 @@ import PortaModel from '../model/porta'
 
 
 export default function Home() {
-  const p1=new PortaModel(1, false, true)
+  const [p1,setP1]=useState(new PortaModel(1))
 
   return (
     <>
       <div className={styles.main}>
-      <Porta value={p1} onChangee={novaPorta => console.log(novaPorta)}/>
+      <Porta value={p1} onChangee={novaPorta => setP1(novaPorta)}/>
       </div>
     </>
   )
