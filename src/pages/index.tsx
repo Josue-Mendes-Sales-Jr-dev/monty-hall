ede5d8b20ec952c8d4504b99cea33f3222a67043
Josué Mendes <109279870+Josue-Mendes-Sales-Jr-dev@users.noreply.github.com>
2023-05-02 14:49:53 -0300

    add new styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,23 +6,25 @@ import Link from 'next/link'
 export default function Home() {
 
   return(
-       <div className={styles.main}>
-       <div className={styles.Qd1}>
-         <Cartao Title="Monty Hall"/>
-       </div>
-       <div className={styles.Qd2}>
-         <Cartao Title="" />
-       </div>
-       <div className={styles.Qd3}>
-         <Cartao Title="" />
-       </div>
-       <div className={styles.Qd4}>
-         <Cartao Title="">
-          <Link href="/jogo/10/4">
-            Iniciar
-          </Link>
-          </Cartao>
-       </div>   
-        </div>
+     <div className={styles.box}>
+         <div className={styles.main}>
+         <div className={styles.Qd1}>
+           <Cartao Title="Monty Hall"/>
+         </div>
+         <div className={styles.Qd2}>
+           <Cartao Title="" />
+         </div>
+         <div className={styles.Qd3}>
+           <Cartao Title="" />
+         </div>
+         <div className={styles.Qd4}>
+           <Cartao Title="">
+            <Link href="/jogo/10/4">
+              Iniciar
+            </Link>
+            </Cartao>
+         </div>
+          </div>
+  </div>
   )
 }
